ebdbaa58393eee3ba37f6c7e0928d36d1769415e
Sosnin Denis <Sosnin.den@gmail.com>
2022-01-07 23:30:07 +0300

    Continua  to practice with git by xCode - create new file LoginFromController and new view

---
+++ ChatALittle/LoginFormController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginFormController.swift
+//  ChatALittle
+//
+//  Created by Денис Соснин on 07.01.2022.
+//
+
+import UIKit
+
+class LoginFormController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
